70cd91acea3fd90e7556c285ee5f1d22d6956dbd
Avinash Kariya <avinashkariya05910@gmail.com>
2024-02-19 22:29:55 +0530

    project bootstraping

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Create virtual environment
+python -m venv myenv
+
+# Activate virtual environment
+source myenv/bin/activate
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
